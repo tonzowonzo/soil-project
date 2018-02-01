94433a888b141032f3e24b77a649d5361aaa2de2
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-02-01 23:36:36 +0100

    Tree classifier for USDA classification

---
+++ soilDataforClassifUSDA.py
@@ -0,0 +1,170 @@
+# Trying to classify soil based on world soil data
+# Importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import argmax
+
+# Load in csv dataset
+df = pd.read_excel('HWSD_DATA.xlsx')
+df = df[df['ISSOIL'].isin([1])] # Drop all the non-soils
+df = df[df.SU_CODE90.notnull()] # Drop all non-classified soils.
+df['USDA_SOIL_CLASS'] = np.nan
+# Get rid of all of the useless columns
+df = df.iloc[:, 7:]
+
+# Fill NaN values with medians.
+df.fillna(df.median(), inplace=True)
+# Add USDA soil classes.
+def USDA_LABEL(row):
+    if row in ['FL', 'RG', 'GL']:
+        return 'Entisol'
+    if row in ['AC']:
+        return 'Ultisol'
+    if row in ['CM']:
+        return 'Inceptisol'
+    if row in ['FR']:
+        return 'Oxisol'
+    if row in ['HS']:
+        return 'Histosol'
+    if row in ['CL', 'GY']:
+        return 'Aridisol'
+    if row in ['AL', 'VR']:
+        return 'Vertisol'
+    if row in ['PZ']:
+        return 'Spodosol'
+    if row in ['PD', 'SN', 'PL', 'LV', 'LX']:
+        return 'Alfisol'
+    if row in ['CH', 'GR', 'KS', 'PH', 'SC']:
+        return 'Mollisol'
+    if row in ['AN']:
+        return 'Andisol'
+    else:
+        return np.NaN
+
+
+# Add new feature of absolute soil class
+df['SOIL_CLASS'] = [x[:2] for x in df['SU_SYM90']]
+# Amount of each type of soil
+
+# Add USDA soil class.
+df['USDA_SOIL_CLASS'] = [USDA_LABEL(x) for x in df['SOIL_CLASS']]
+
+# Get rid of more columns I don't need
+df = df.iloc[:, 6:-1]
+
+# Drop final NaNs
+df = df.dropna()
+'''
+Below shows that most our soil types have sample sizes of <20 and are largely spread from
+0 to 200, there is one soil type I which almost contains 1000 samples however.
+'''
+df['USDA_SOIL_CLASS'].unique()
+x = df['USDA_SOIL_CLASS'].value_counts()
+plt.hist(x, bins=20)
+
+'''
+Below shows the distribution of drainage types with 1 being very poor drainage and
+6 being somewhat excessive. The most common class is 4 which is a moderately-well drained
+soil.
+'''
+plt.hist(df['DRAINAGE'], bins=6)
+
+'''
+Below shows the distribution of the texture types. 2 is the most common texture by
+a significant margin, 2 is a medium texture.
+'''
+plt.hist(df['T_TEXTURE'], bins=4)
+
+# Encoding categorical variable for soil class
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+le = LabelEncoder()
+y = df.iloc[:, -1]
+y = le.fit_transform(y)
+y = y.reshape(-1, 1) # It needs reshaping for the onehotencoder to work.
+# One hot encode the variable
+onehot = OneHotEncoder(sparse=False)
+y = onehot.fit_transform(y)
+
+x = df.iloc[:, 0:-1]
+columns = list(x.columns.values)
+
+x = df[['DRAINAGE', 'T_USDA_TEX_CLASS', 'T_BULK_DENSITY', 'T_PH_H2O']]
+# Some feature scaling
+#from sklearn.preprocessing import StandardScaler
+#sc_x = StandardScaler()
+#x = sc_x.fit_transform(x) #need to fit and then xform
+#Rename columns with original names
+#x.columns = columns
+# Create x and y train and test sets.
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Random forest.
+from sklearn.ensemble import RandomForestClassifier
+rand_for = RandomForestClassifier()
+rand_for.fit(X_train, y_train)
+
+# Try model on the test set.
+from sklearn.model_selection import cross_val_predict, cross_val_score
+cross_val_score(rand_for, X_train, y_train, cv=3, scoring='accuracy')
+
+# Performing a grid search to see if we can achieve a better model.
+from sklearn.model_selection import GridSearchCV
+param_grid =  {'n_estimators': [5, 6, 7], 'bootstrap': [False],
+               'max_features':[None]}
+rand_for = RandomForestClassifier()
+grid_search = GridSearchCV(rand_for, param_grid, cv=5,
+                           scoring='accuracy')
+grid_search.fit(X_train, y_train)
+print(grid_search.best_params_)
+rand_for = RandomForestClassifier(max_features=None, n_estimators=5, bootstrap=False)
+rand_for.fit(X_train, y_train)
+# Check confusion matrix to see if the model is just selection most likely classes.
+y_pred = rand_for.predict(X_test)
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+recall = recall_score(y_test, y_pred, average='weighted')
+precision = precision_score(y_test, y_pred, average='weighted')
+accuracy = accuracy_score(y_test, y_pred)
+print('Recall: {}, Precision: {}, Accuracy: {}'.format(recall, precision, accuracy))
+
+# Save the model
+from sklearn.externals import joblib
+joblib.dump(rand_for, 'random_forest_soil.pkl')
+
+# Reload the model
+joblib.load('random_forest_soil.pkl')
+
+# What about if we do some feature selection
+#from sklearn.feature_selection import SelectFromModel
+#print(rand_for.feature_importances_)
+#model = SelectFromModel(rand_for, prefit=True)
+#X_train_new = model.transform(X_train)
+#X_test_new = model.transform(X_test)
+#new_rand_for = RandomForestClassifier(max_features=None, n_estimators=5, bootstrap=False)
+#new_rand_for.fit(X_train_new, y_train)
+
+# Feature selection with k selections
+#from sklearn.feature_selection import SelectKBest
+#from sklearn.feature_selection import chi2
+#x_new = SelectKBest(chi2, k=5).fit_transform(x, y)
+#x_new = pd.DataFrame(x_new)
+#x_new.columns = ['Topsoil Organic Carbon', 'Topsoil CEC',
+#                 'Subsoil Organic Carbon', 'Subsoil CEC', 'Subsoil TEB']
+#new_rand_for = RandomForestClassifier(max_features=None, n_estimators=5, bootstrap=False)
+#from sklearn.model_selection import train_test_split
+#X_train_new, X_test_new, y_train, y_test = train_test_split(x_new, y, test_size=0.2, random_state=42)
+#new_rand_for.fit(X_train_new, y_train)
+#y_pred = new_rand_for.predict(X_test_new)
+
+#from sklearn.metrics import accuracy_score
+#from sklearn.metrics import recall_score
+#from sklearn.metrics import precision_score
+#recall = recall_score(y_test, y_pred, average='weighted')
+#precision = precision_score(y_test, y_pred, average='weighted')
+#accuracy = accuracy_score(y_test, y_pred)
+#print('Recall: {}, Precision: {}, Accuracy: {}'.format(recall, precision, accuracy))
