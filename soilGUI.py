32d21f2cf72c49d56e3ada49aa3c66e0db20ca93
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-02-20 23:45:25 +0100

    fixed double click analyse bug

--- soilGUI.py
+++ soilGUI.py
@@ -26,6 +26,7 @@ class SoilGui(QMainWindow):
         self.initUI()
         
     def initUI(self):
+        self.isPressed = False
         # Import button
         btn1 = QPushButton('Import', self) # Button object, called import.
         btn1.clicked.connect(self.getFile)
@@ -37,7 +38,7 @@ class SoilGui(QMainWindow):
         btn2.resize(btn2.sizeHint()) 
         btn2.move(150, 530) 
         # Analyse button
-        btn3 = QPushButton('Analyse', self) 
+        btn3 = QPushButton('Analyse', self)
         btn3.clicked.connect(self.predictImage)
         btn3.resize(btn2.sizeHint()) 
         btn3.move(250, 530) 
@@ -112,6 +113,7 @@ class SoilGui(QMainWindow):
         '''
         Fetches an image file from a file menu and display
         '''
+        self.isPressed = False
         l1 = QLabel(self)
         image = QFileDialog.getOpenFileName(None,'OpenFile','',"Image file(*.jpg *.png)")
         self.imagePath = image[0]
@@ -128,158 +130,159 @@ class SoilGui(QMainWindow):
         '''
         Predicts the image class based on the input from keras CNN model.
         '''
-        self.predictLabel = None
-        self.predictLabel = QLabel(self)
-        self.predictLabel.move(50, 430)
-        self.predictLabel.resize(250, 100)
-        self.predictLabel.show()
-        img = image.load_img(self.imagePath, target_size=(299, 299))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = x / 255
-        # Ensemble with 2 models
-        pred1 = model.predict(x)
-        pred2 = model2.predict(x)
-        pred = (pred1 + pred2) / 2
-        print(pred)
-#        indexes = np.argpartition(pred, 10)[-2:]
-        indexes = np.argsort(pred)[-2:]
-        print(indexes)
-        # Predict multi class classification
-        if indexes[0][10] == 0:
-            soil1 = 'Alfisol'
-            probability1 = pred[0][0] * 100
-        elif indexes[0][10] == 1:
-            soil1 = 'Andisol'
-            probability1 = pred[0][1] * 100
-        elif indexes[0][10] == 2:
-            soil1 = 'Aridisol'
-            probability1 = pred[0][2] * 100
-        elif indexes[0][10] == 3:
-            soil1 = 'Entisol'
-            probability1 = pred[0][3] * 100
-        elif indexes[0][10] == 4:
-            soil1 = 'Histosol'
-            probability1 = pred[0][4] * 100
-        elif indexes[0][10] == 5:
-            soil1 = 'Inceptisol'
-            probability1 = pred[0][5] * 100
-        elif indexes[0][10] == 6:
-            soil1 = 'Mollisol'
-            probability1 = pred[0][6] * 100
-        elif indexes[0][10] == 7:
-            soil1 = 'Oxisol'
-            probability1 = pred[0][7] * 100
-        elif indexes[0][10] == 8:
-            soil1 = 'Spodosol'
-            probability1 = pred[0][8] * 100
-        elif indexes[0][10] == 9:
-            soil1 = 'Ultisol'
-            probability1 = pred[0][9] * 100
-        elif indexes[0][10] == 10:
-            soil1 = 'Vertisol'
-            probability1 = pred[0][10] * 100
-        print(soil1, probability1)
-        if indexes[0][9] == 0:
-            soil2 = 'Alfisol'
-            probability2 = pred[0][0] * 100
-        elif indexes[0][9] == 1:
-            soil2 = 'Andisol'
-            probability2 = pred[0][1] * 100
-        elif indexes[0][9] == 2:
-            soil2 = 'Aridisol'
-            probability2 = pred[0][2] * 100
-        elif indexes[0][9] == 3:
-            soil2 = 'Entisol'
-            probability2 = pred[0][3] * 100
-        elif indexes[0][9] == 4:
-            soil2 = 'Histosol'
-            probability2 = pred[0][4] * 100
-        elif indexes[0][9] == 5:
-            soil2 = 'Inceptisol'
-            probability2 = pred[0][5] * 100
-        elif indexes[0][9] == 6:
-            soil2 = 'Mollisol'
-            probability2 = pred[0][6] * 100
-        elif indexes[0][9] == 7:
-            soil2 = 'Oxisol'
-            probability2 = pred[0][7] * 100
-        elif indexes[0][9] == 8:
-            soil2 = 'Spodosol'
-            probability2 = pred[0][8] * 100
-        elif indexes[0][9] == 9:
-            soil2 = 'Ultisol'
-            probability2 = pred[0][9] * 100
-        elif indexes[0][9] == 10:
-            soil2 = 'Vertisol'
-            probability2 = pred[0][10] * 100
-        print(soil2, probability2)
-
-        self.predictLabel.setText('This is a {} with {}% probability \nOr a {} with {}% probability'.format(soil1,
-                                  str(round(probability1, 2)), soil2, str(round(probability2, 2))))
-        print('This is a {} with {}% probability \n Or a {} with {}% probability'.format(soil1,
-                                  str(round(probability1, 2)), soil2, str(round(probability2, 2))))
-        pH = None
-        bulk_density = None
-        drainage_class = None
-        USDA_texture_class = None
-        try:
-            if 0 < float(self.pH.text()) < 15:
-                pH = float(self.pH.text())
-                print(pH)                    
-        except:
-            pass        
-        try:
-            if 0 < float(self.bulkd.text()) < 4:
-                bulk_density = float(self.bulkd.text())
-                print(bulk_density)
-        except ValueError:
-            pass            
-        if self.drainage.text().isdigit():
-            drainage_class = int(self.drainage.text())
-            if drainage_class < 8 and drainage_class > 0:
-                print(drainage_class)
-        if self.texture.text().isdigit() and 0 < int(self.texture.text()) < 14:
-            USDA_texture_class = int(self.drainage.text())
-            print(USDA_texture_class)
-            
-        if None in [pH, bulk_density, drainage_class, USDA_texture_class]:
-            print('You did not input a or several numbers')
-        else:
-            nums_for_prediction = np.array([drainage_class, USDA_texture_class, bulk_density, pH])
-            nums_for_prediction = nums_for_prediction.reshape(1, -1)
-            prediction_forest = rand_forest_soil.predict(nums_for_prediction)
-            soil_type = np.argmax(prediction_forest)
-            print(prediction_forest)
-            
-            if soil_type == 0:
-                soil = 'Random forest classifier: This is an Alfisol'
-            if soil_type == 1:
-                soil = 'Random forest classifier: This is an Andisol'
-            elif soil_type == 2:
-                soil = 'Random forest classifier: This is an Aridisol'
-            elif soil_type == 3:
-                soil = 'Random forest classifier: This is an Entisol'
-            elif soil_type == 4:
-                soil = 'Random forest classifier: This is a Histosol'
-            elif soil_type == 5:
-                soil = 'Random forest classifier: This is an Inceptisol'
-            elif soil_type == 6:
-                soil = 'Random forest classifier: This is a Mollisol'
-            elif soil_type == 7:
-                soil = 'Random forest classifier: This is an Oxisol'
-            elif soil_type == 8:
-                soil = 'Random forest classifier: This is a Spodosol'
-            elif soil_type == 9:
-                soil ='Random forest classifier: This is an Ultisol'
-            elif soil_type == 10:
-                soil = 'Random forest classifier: This is a Vertisol'
+        if self.isPressed == False:
+            self.isPressed = True
+            self.predictLabel = QLabel(self)
+            self.predictLabel.move(50, 430)
+            self.predictLabel.resize(250, 100)
+            self.predictLabel.show()
+            img = image.load_img(self.imagePath, target_size=(299, 299))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = x / 255
+            # Ensemble with 2 models
+            pred1 = model.predict(x)
+            pred2 = model2.predict(x)
+            pred = (pred1 + pred2) / 2
+            print(pred)
+    #        indexes = np.argpartition(pred, 10)[-2:]
+            indexes = np.argsort(pred)[-2:]
+            print(indexes)
+            # Predict multi class classification
+            if indexes[0][10] == 0:
+                soil1 = 'Alfisol'
+                probability1 = pred[0][0] * 100
+            elif indexes[0][10] == 1:
+                soil1 = 'Andisol'
+                probability1 = pred[0][1] * 100
+            elif indexes[0][10] == 2:
+                soil1 = 'Aridisol'
+                probability1 = pred[0][2] * 100
+            elif indexes[0][10] == 3:
+                soil1 = 'Entisol'
+                probability1 = pred[0][3] * 100
+            elif indexes[0][10] == 4:
+                soil1 = 'Histosol'
+                probability1 = pred[0][4] * 100
+            elif indexes[0][10] == 5:
+                soil1 = 'Inceptisol'
+                probability1 = pred[0][5] * 100
+            elif indexes[0][10] == 6:
+                soil1 = 'Mollisol'
+                probability1 = pred[0][6] * 100
+            elif indexes[0][10] == 7:
+                soil1 = 'Oxisol'
+                probability1 = pred[0][7] * 100
+            elif indexes[0][10] == 8:
+                soil1 = 'Spodosol'
+                probability1 = pred[0][8] * 100
+            elif indexes[0][10] == 9:
+                soil1 = 'Ultisol'
+                probability1 = pred[0][9] * 100
+            elif indexes[0][10] == 10:
+                soil1 = 'Vertisol'
+                probability1 = pred[0][10] * 100
+            print(soil1, probability1)
+            if indexes[0][9] == 0:
+                soil2 = 'Alfisol'
+                probability2 = pred[0][0] * 100
+            elif indexes[0][9] == 1:
+                soil2 = 'Andisol'
+                probability2 = pred[0][1] * 100
+            elif indexes[0][9] == 2:
+                soil2 = 'Aridisol'
+                probability2 = pred[0][2] * 100
+            elif indexes[0][9] == 3:
+                soil2 = 'Entisol'
+                probability2 = pred[0][3] * 100
+            elif indexes[0][9] == 4:
+                soil2 = 'Histosol'
+                probability2 = pred[0][4] * 100
+            elif indexes[0][9] == 5:
+                soil2 = 'Inceptisol'
+                probability2 = pred[0][5] * 100
+            elif indexes[0][9] == 6:
+                soil2 = 'Mollisol'
+                probability2 = pred[0][6] * 100
+            elif indexes[0][9] == 7:
+                soil2 = 'Oxisol'
+                probability2 = pred[0][7] * 100
+            elif indexes[0][9] == 8:
+                soil2 = 'Spodosol'
+                probability2 = pred[0][8] * 100
+            elif indexes[0][9] == 9:
+                soil2 = 'Ultisol'
+                probability2 = pred[0][9] * 100
+            elif indexes[0][9] == 10:
+                soil2 = 'Vertisol'
+                probability2 = pred[0][10] * 100
+            print(soil2, probability2)
+    
+            self.predictLabel.setText('This is a {} with {}% probability \nOr a {} with {}% probability'.format(soil1,
+                                      str(round(probability1, 2)), soil2, str(round(probability2, 2))))
+            print('This is a {} with {}% probability \n Or a {} with {}% probability'.format(soil1,
+                                      str(round(probability1, 2)), soil2, str(round(probability2, 2))))
+            pH = None
+            bulk_density = None
+            drainage_class = None
+            USDA_texture_class = None
+            try:
+                if 0 < float(self.pH.text()) < 15:
+                    pH = float(self.pH.text())
+                    print(pH)                    
+            except:
+                pass        
+            try:
+                if 0 < float(self.bulkd.text()) < 4:
+                    bulk_density = float(self.bulkd.text())
+                    print(bulk_density)
+            except ValueError:
+                pass            
+            if self.drainage.text().isdigit():
+                drainage_class = int(self.drainage.text())
+                if drainage_class < 8 and drainage_class > 0:
+                    print(drainage_class)
+            if self.texture.text().isdigit() and 0 < int(self.texture.text()) < 14:
+                USDA_texture_class = int(self.drainage.text())
+                print(USDA_texture_class)
                 
-            self.TreePredictLabel = QLabel(self)
-            self.TreePredictLabel.move(580, 330)
-            self.TreePredictLabel.resize(300, 100)
-            self.TreePredictLabel.setText(soil)
-            self.TreePredictLabel.show()
+            if None in [pH, bulk_density, drainage_class, USDA_texture_class]:
+                print('You did not input a or several numbers')
+            else:
+                nums_for_prediction = np.array([drainage_class, USDA_texture_class, bulk_density, pH])
+                nums_for_prediction = nums_for_prediction.reshape(1, -1)
+                prediction_forest = rand_forest_soil.predict(nums_for_prediction)
+                soil_type = np.argmax(prediction_forest)
+                print(prediction_forest)
+                
+                if soil_type == 0:
+                    soil = 'Random forest classifier: This is an Alfisol'
+                if soil_type == 1:
+                    soil = 'Random forest classifier: This is an Andisol'
+                elif soil_type == 2:
+                    soil = 'Random forest classifier: This is an Aridisol'
+                elif soil_type == 3:
+                    soil = 'Random forest classifier: This is an Entisol'
+                elif soil_type == 4:
+                    soil = 'Random forest classifier: This is a Histosol'
+                elif soil_type == 5:
+                    soil = 'Random forest classifier: This is an Inceptisol'
+                elif soil_type == 6:
+                    soil = 'Random forest classifier: This is a Mollisol'
+                elif soil_type == 7:
+                    soil = 'Random forest classifier: This is an Oxisol'
+                elif soil_type == 8:
+                    soil = 'Random forest classifier: This is a Spodosol'
+                elif soil_type == 9:
+                    soil ='Random forest classifier: This is an Ultisol'
+                elif soil_type == 10:
+                    soil = 'Random forest classifier: This is a Vertisol'
+                    
+                self.TreePredictLabel = QLabel(self)
+                self.TreePredictLabel.move(580, 330)
+                self.TreePredictLabel.resize(300, 100)
+                self.TreePredictLabel.setText(soil)
+                self.TreePredictLabel.show()
             
 
 
@@ -300,5 +303,3 @@ if __name__ == '__main__':
         app.setStyle(QStyleFactory.create('Windows'))
     w = SoilGui() # Opens an instance of the SoilGui class.
     sys.exit(app.exec_()) # Allows a clean exit of the application.
-
-
